90046d2aa36a1a969cecc92df91441d4e8610e6b
joaquin848 <joaky848@gmail.com>
2025-06-24 18:21:16 -0300

    Agrego nuevas funciones al login

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Favourite
+
+@admin.register(Favourite)
+class FavouriteAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id', 'user', 'types', 'height', 'weight', 'base_experience')
+    search_fields = ('name', 'types', 'user__username')
+    list_filter = ('types',)
+
