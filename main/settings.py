0291d6a6d72e027d45bcf7b8a1750bcc1cae8436
Tobias Bernini <tobiasbernini@gmail.com>
2025-06-28 16:16:54 -0300

    no contras

--- main/settings.py
+++ main/settings.py
@@ -127,6 +127,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'tobiasbernini@gmail.com'            
-EMAIL_HOST_PASSWORD = 'ixgfmxacwitbdfei'              
+EMAIL_HOST_USER = ''            
+EMAIL_HOST_PASSWORD = ''              
 
